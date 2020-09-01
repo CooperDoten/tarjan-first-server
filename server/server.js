6792bdaacc3c1d68944998e1325f4d971c4a342e
Cooper Doten <cooper.doten@gmail.com>
2020-09-01 15:31:23 -0500

    serve static assets.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const express = require('express');
 
 //express exports function that makes a new app
 const app = express();
+//for serving static files
+app.use(express.static('server/public'));
+//when I get a request at '/hello'
+//send back a response of 'Hello world!'
+app.get('/hello', function(req, res) {
+    res.send(`<h1>Hello World!</h1>`);
+});
 
 // start up the server
 
